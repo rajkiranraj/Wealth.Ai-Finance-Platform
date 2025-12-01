2e5c47522e532dadfed579f1a9cd689f0e4ef4c7
Raj Kiran <rajkiranraj0000@gmail.com>
2025-12-01 22:48:32 +0530

    Added auth using clerk

---
+++ components/header.jsx
@@ -0,0 +1,21 @@
+import { memo } from 'react';
+import { ClerkProvider,SignedOut,SignedIn,SignInButton,SignUpButton,UserButton } from "@clerk/nextjs";
+const Header = () => {
+  return (
+    <div>
+     <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+    </div>
+  );
+};
+
+export default memo(Header);

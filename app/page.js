cd00ca11201028e7beabfd0eefe6c4d007d0ac65
RUDRA RAJ KIRAN <rajkiranraj0000@gmail.com>
2026-04-24 20:43:04 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import {
 } from "@/data/landing";
 import HeroSection from "@/components/hero";
 import Link from "next/link";
-
+//Landing page
 const LandingPage = () => {
   return (
     <div className="min-h-screen bg-white">
